e337d7932a97498261c4e6cdbb96bdd945171973
Patrick Meaney <patrick.wm.meaney@gmail.com>
2023-08-18 16:22:20 -0500

    added passportjs oauth2 for google

--- app.js
+++ app.js
@@ -3,15 +3,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.json');
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'SECRET' 
+}));
+
+
+var passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +36,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/v1/users', usersRouter);
+
+// swagger route
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.render('pages/auth');
+});
+
 const process = require('../config-server-env.json')
 const environment = process.env.NODE_ENV
 const knex_config = require('../knexfile')
 const database = require('knex')(knex_config[environment])
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 async function selectAllEmployees() {
   const results = await database('employees').select('*')
   return results
 }
 
-router.get("/employees", async (req, res) => {
+router.get("/v1/employees", async (req, res) => {
   try {
     console.log('running query...')
     const dataObject = await selectAllEmployees();
@@ -28,5 +29,52 @@ router.get("/employees", async (req, res) => {
     res.status(500).json({message: "Error getting posts"})
   }
 });
+
+// ########### Auth section: PassportJS
+
+var passport = require('passport');
+var userProfile;
+
+router.get('/success', (req, res) => {
+  res.render('pages/success', {user: userProfile});
+});
+
+router.get('/error', (req, res) => res.send("error logging in"));
+ 
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+ 
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+// ########### PassportJS Google AUTH 
+ 
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GOOGLE_CLIENT_ID = process.env.DEV_GOOGLE_OAUTH_CLIENTID
+const GOOGLE_CLIENT_SECRET = process.env.DEV_GOOGLE_OAUTH_CLIENTSECRET;
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile=profile;
+      return done(null, userProfile);
+  }
+));
+ 
+router.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+router.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    // Successful authentication, redirect success.
+    res.redirect('/success');
+  });
+
 
 module.exports = router;
