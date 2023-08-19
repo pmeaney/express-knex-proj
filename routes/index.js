c074f2af930386d1ef497b56cfea7c4b0c89cd5d
Patrick Meaney <patrick.wm.meaney@gmail.com>
2023-08-19 11:32:11 -0500

    added passport configs for facebook sign in

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,53 @@
 var express = require('express');
 var router = express.Router();
 
+const process = require('../config-server-env.json')
+const environment = process.env.NODE_ENV
+const knex_config = require('../knexfile')
+const database = require('knex')(knex_config[environment])
+
+
+var passport = require('passport');
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+const FacebookStrategy = require('passport-facebook').Strategy;
+const FACEBOOK_OAUTH_CLIENT_ID = process.env.DEV_FACEBOOK_OAUTH_CLIENTID
+const FACEBOOK_OAUTH_CLIENT_SECRET = process.env.DEV_FACEBOOK_OAUTH_CLIENTSECRET
+const FACEBOOK_OAUTH_BACLLBACK_URL = process.env.DEV_FACEBOOK_OAUTH_CALLBACKURL
+
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GOOGLE_OAUTH_CLIENT_ID = process.env.DEV_GOOGLE_OAUTH_CLIENTID
+const GOOGLE_OAUTH_CLIENT_SECRET = process.env.DEV_GOOGLE_OAUTH_CLIENTSECRET;
+const GOOGLE_OAUTH_CALLBACK_URL = process.env.DEV_GOOGLE_OAUTH_CALLBACKURL;
+
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_OAUTH_CLIENT_ID,
+  clientSecret: GOOGLE_OAUTH_CLIENT_SECRET,
+  callbackURL: GOOGLE_OAUTH_CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+}
+));
+passport.use(new FacebookStrategy({
+  clientID: FACEBOOK_OAUTH_CLIENT_ID,
+  clientSecret: FACEBOOK_OAUTH_CLIENT_SECRET,
+  callbackURL: FACEBOOK_OAUTH_BACLLBACK_URL
+}, function (accessToken, refreshToken, profile, done) {
+  return done(null, profile);
+}
+));
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
   res.render('pages/auth');
 });
-
-const process = require('../config-server-env.json')
-const environment = process.env.NODE_ENV
-const knex_config = require('../knexfile')
-const database = require('knex')(knex_config[environment])
 
 async function selectAllEmployees() {
   const results = await database('employees').select('*')
@@ -31,50 +68,60 @@ router.get("/v1/employees", async (req, res) => {
 });
 
 // ########### Auth section: PassportJS
-
-var passport = require('passport');
-var userProfile;
-
-router.get('/success', (req, res) => {
-  res.render('pages/success', {user: userProfile});
+router.get('/loggedin_profile', isLoggedIn, (req, res) => {
+  console.log("req.isAuthenticated()?", req.isAuthenticated())
+  res.render('pages/success', { user: req.user});
 });
 
 router.get('/error', (req, res) => res.send("error logging in"));
  
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
- 
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
+// ########### PassportJS Google Auth 
 
-// ########### PassportJS Google AUTH 
- 
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const GOOGLE_CLIENT_ID = process.env.DEV_GOOGLE_OAUTH_CLIENTID
-const GOOGLE_CLIENT_SECRET = process.env.DEV_GOOGLE_OAUTH_CLIENTSECRET;
-
-passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-      userProfile=profile;
-      return done(null, userProfile);
-  }
-));
- 
 router.get('/auth/google', 
   passport.authenticate('google', { scope : ['profile', 'email'] }));
  
 router.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/error' }),
-  function(req, res) {
-    // Successful authentication, redirect success.
+  passport.authenticate('google',
+  { failureRedirect: '/error',
+    successRedirect: '/loggedin_profile' }),
+  /* The above is shorthand. Here's another option-- For either (i assume)
+    failure or success, we can run a fn.  In the shorthand, we just specify the redirect url
+
+    E.g. Authenticate via google strat.
+    if it fails (can probably optionally replace w/ fn), redirect to failure page.
+    otherwise, run this fn (redirect to success page) 
+    passport.authenticate('google',
+    { failureRedirect: '/error' }),
+    function(req, res) {
+    Successful authentication, redirect success.
     res.redirect('/success');
-  });
+    }
+  */
+  );
+
+// ############# PassportJS Facebook Auth
+
+router.get('/auth/facebook', 
+  passport.authenticate('facebook', {
+    scope:['public_profile', 'email']
+  })
+);
+
+router.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {
+    successRedirect: '/loggedin_profile',
+    failureRedirect: '/error'
+  })
+);
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+    // if user is authenticated, next (i.e. proceed to req/res fn)
+    return next();
+  // otherwise, direct to main page.
+  res.redirect('/');
+}
 
 
 module.exports = router;
+
